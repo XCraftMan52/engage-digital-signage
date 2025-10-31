67f2441d87130587a42f1dfb535f4a8d636bc349
Lucas Webber <lucas@lucaswebber.dev>
2025-10-31 18:31:13 -0400

    refactor: update Next.js configuration and enhance homepage to display today's events

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'se-images.campuslabs.com',
+        port: '',
+        pathname: '/clink/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.campuslabsengage.com',
+        port: '',
+        pathname: '/discovery/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
